6fd56c81c05816fa507d717023b219705bcb5ec3
OsmondRaye <osmondraye@gmail.com>
2024-03-06 02:48:50 +0800

    second commit

--- users/users.controller.js
+++ users/users.controller.js
@@ -55,7 +55,7 @@ function createSchema(req, res, next) {
         firstName: Joi.string().required(),
         lastName: Joi.string().required(),
         role: Joi.string().valid(Role.Admin, Role.User).required(),
-        email: Joi.string().required(),
+        email: Joi.string().email().required(),
         password: Joi.string().min(6).required(),
         confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
     });
@@ -68,7 +68,7 @@ function updateSchema(req, res, next) {
         firstName: Joi.string().empty(''),
         lastName: Joi.string().empty(''),
         role: Joi.string().valid(Role.Admin, Role.User).empty(''),
-        email: Joi.string().empty(''),
+        email: Joi.string().email().empty(''),
         password: Joi.string().min(6).empty(''),
         confirmPassword: Joi.string().valid(Joi.ref('password')).empty(''),
     }).with('password', 'confirmPassword');
